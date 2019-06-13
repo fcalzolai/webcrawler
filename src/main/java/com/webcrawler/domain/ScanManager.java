a4680072dbe54ea322cdcada2a3c9983a0757ef4
francesco.calzolai <francesco.calzolai@lloydsbanking.com>
2019-06-13 11:28:25 +0100

    Change ThradPool

--- src/main/java/com/webcrawler/domain/ScanManager.java
+++ src/main/java/com/webcrawler/domain/ScanManager.java
@@ -11,8 +11,9 @@ import java.util.Set;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ScanManager {
 
@@ -22,14 +23,14 @@ public class ScanManager {
     private final String baseUrl;
     private final BlockingQueue<String> toBeScanned;
     private final Map<String, Set<String>> links;
-    private final ExecutorService executor;
+    private final ScheduledExecutorService executor;
 
     public ScanManager(String baseUrl) {
         setLogLevel();
         this.baseUrl = baseUrl;
         links = new ConcurrentHashMap<>();
         toBeScanned = new ArrayBlockingQueue<>(CAPACITY);
-        executor = Executors.newFixedThreadPool(N_THREADS);
+        executor = Executors.newScheduledThreadPool(N_THREADS);
 
         initThreads();
         toBeScanned.add(baseUrl);
@@ -39,7 +40,8 @@ public class ScanManager {
         LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
         loggerContext.getLogger("io.netty").setLevel(Level.INFO);
         loggerContext.getLogger("reactor.netty").setLevel(Level.INFO);
-        loggerContext.getLogger("org.springframework.web.reactive.function.client").setLevel(Level.INFO);
+        loggerContext.getLogger("org.springframework.core.codec").setLevel(Level.INFO);
+        loggerContext.getLogger("org.springframework.web").setLevel(Level.INFO);
     }
 
     public Map<String, Set<String>> getLinks() {
@@ -51,21 +53,22 @@ public class ScanManager {
     }
 
     public void newLinksFound(String src, Set<String> newLinks) {
-        new Thread(() -> newLinks.forEach(dest -> {
-            try {
-                if (isInternalLink(dest)) {
-                    if (shouldBeScanned(dest)) {
-                        toBeScanned.put(dest);  //Blocking method
-                    }
+        new Thread(() -> {
+            newLinks.forEach(dest -> {
+                try {
+                    if (isInternalLink(dest)) {
+                        if (shouldBeScanned(dest)) {
+                            toBeScanned.put(dest);  //Blocking method
+                        }
 
-                    links.computeIfAbsent(src, s -> new HashSet<>())
-                            .add(dest);
-                    System.err.println(Thread.currentThread().getName() + "-[" + toBeScanned.size() + "]" + src + " --> " + dest);
+                        links.computeIfAbsent(src, s -> new HashSet<>())
+                                .add(dest);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        })).start();
+            });
+        }).start();
     }
 
     private boolean shouldBeScanned(String url){
@@ -78,17 +81,21 @@ public class ScanManager {
 
     private void initThreads() {
         for (int i = 0; i < N_THREADS; i++) {
-            executor.execute(new Thread("T"+i) {
-                @Override
-                public void run() {
-                    try {
-                        scanFirstElement();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            });
+            executor.scheduleWithFixedDelay(getTask(i), 0, 1, TimeUnit.SECONDS);
         }
+    }
+
+    private Thread getTask(int i) {
+        return new Thread("T"+i) {
+            @Override
+            public void run() {
+                try {
+                    scanFirstElement();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 
     private boolean isInternalLink(String dest) {
@@ -107,12 +114,11 @@ public class ScanManager {
         Finder finder = new Finder();
         Scanner scanner = new Scanner(finder);
 
-        while (true) {
-            String path = toBeScanned.take();  //Blocking invocation
-            finder.setConsumer(links -> newLinksFound(path, links));
-            String url = (path.contains(baseUrl))? path : baseUrl + path;
-            scanner.scan(url);
-        }
+        String path = toBeScanned.take();  //Blocking invocation
+        finder.setConsumer(links -> newLinksFound(path, links));
+        String url = (path.contains(baseUrl)) ? path : baseUrl + path;
+        scanner.scan(url);
+        System.err.println("["+Thread.currentThread().getName()+"] - toBeScanned["+toBeScanned.size()+"] - links["+links.size()+"]");
     }
 
     public void shoutDown(){
